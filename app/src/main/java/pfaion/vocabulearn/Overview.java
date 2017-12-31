318bb87345b5549903bbed8db02629ecbea08230
Patrick Faion <pfaion@uos.de>
2017-12-31 15:22:55 +0100

    Initial commit (bottom navigation)

---
+++ app/src/main/java/pfaion/vocabulearn/Overview.java
@@ -0,0 +1,45 @@
+package pfaion.vocabulearn;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class Overview extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_study:
+                    mTextMessage.setText(R.string.title_study);
+                    return true;
+                case R.id.navigation_folders:
+                    mTextMessage.setText(R.string.title_folders);
+                    return true;
+                case R.id.navigation_smart_sets:
+                    mTextMessage.setText(R.string.title_smart_sets);
+                    return true;
+            }
+            return false;
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_overview);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
