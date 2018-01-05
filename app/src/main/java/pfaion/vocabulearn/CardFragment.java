ac3fc387f7fa292157c04c28d432dfe208f1d71b
Patrick Faion <pfaion@uos.de>
2018-01-05 19:13:42 +0100

    Fragment animations for cardview

---
+++ app/src/main/java/pfaion/vocabulearn/CardFragment.java
@@ -0,0 +1,76 @@
+package pfaion.vocabulearn;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class CardFragment extends Fragment {
+
+    private String text;
+
+    private OnFragmentInteractionListener mListener;
+
+    public CardFragment() {
+    }
+
+    public static CardFragment newInstance(String text) {
+        CardFragment fragment = new CardFragment();
+        Bundle args = new Bundle();
+        args.putString("text", text);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            text = getArguments().getString("text");
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_card, container, false);
+        TextView textView = view.findViewById(R.id.textView5);
+        textView.setText(text);
+        return view;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
--- app/src/main/java/pfaion/vocabulearn/CardViewActivity.java
+++ app/src/main/java/pfaion/vocabulearn/CardViewActivity.java
@@ -1,18 +1,52 @@
 package pfaion.vocabulearn;
 
+import android.app.FragmentTransaction;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
-public class CardViewActivity extends AppCompatActivity {
+public class CardViewActivity extends AppCompatActivity
+implements CardFragment.OnFragmentInteractionListener{
+    public static final String TAG = "Vocabulearn";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_card_view);
+
+
+        findViewById(R.id.imageButton5).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.setCustomAnimations(
+                        R.animator.card_flip_right_in,
+                        R.animator.card_flip_right_out,
+                        R.animator.card_flip_left_in,
+                        R.animator.card_flip_left_out);
+                transaction.replace(R.id.frameLayout, CardFragment.newInstance("Flippedy flooty\nI'm coming for that booty!"));
+                transaction.commit();
+            }
+        });
+
+        findViewById(R.id.imageButton7).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.setCustomAnimations(R.animator.slide_in_left, R.animator.slide_out_right, R.animator.slide_in_right, R.animator.slide_out_left);
+                transaction.replace(R.id.frameLayout, CardFragment.newInstance("You have found ze button!"));
+                transaction.commit();
+            }
+        });
+
+
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.frameLayout, CardFragment.newInstance("Hallo Welt!"));
+        transaction.commit();
     }
 
 
@@ -29,5 +63,10 @@ public class CardViewActivity extends AppCompatActivity {
                             | View.SYSTEM_UI_FLAG_FULLSCREEN
                             | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
         }
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
